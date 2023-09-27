7bd43ce74e8c2e0249cf45d6d32d608448cb03f1
kavya <kkavyassharma@gmail.com>
2023-09-27 09:15:56 +0530

    error solved

--- main.js
+++ main.js
@@ -7,18 +7,16 @@ const updateDetails = (url, title, author) => {
     memeTitle.innerHTML = title;
     memeAuthor.innerHTML = author;
 };
-     const generateMeme = () => {
-        fetch("https://meme-api.com/gimme/wholesomememes")
+    const generateMeme = () => {
+      fetch("https://meme-api.com/gimme/wholesomememes")
         .then((response) => response.json())
         .then((data) => {
-            updateDetails(data.url, data.title, data.author)
-            .catch(error){
-                console.log("Error while fetching the meme: ", error);
-            }
+          updateDetails(data.url, data.title, data.author);
+        })
+        .catch((error) => {
+          console.log("Error while fetching the meme: ", error);
         });
     };
-
-
 
 generateMemeBtn.addEventListener("click", generateMeme);
 generateMeme();
