2dcebe938997ba5d9533cf62eca8d6e4cbd80796
kavya <kkavyassharma@gmail.com>
2023-09-27 09:11:56 +0530

    error handled

--- main.js
+++ main.js
@@ -7,16 +7,17 @@ const updateDetails = (url, title, author) => {
     memeTitle.innerHTML = title;
     memeAuthor.innerHTML = author;
 };
-try{
      const generateMeme = () => {
-        fetch("https://meme-api.com/gimme/wholesomememes").then((response) => response.json()).then((data) => {
+        fetch("https://meme-api.com/gimme/wholesomememes")
+        .then((response) => response.json())
+        .then((data) => {
             updateDetails(data.url, data.title, data.author)
+            .catch(error){
+                console.log("Error while fetching the meme: ", error);
+            }
         });
     };
-}
-catch(err){
-console.log(err);
-}
+
 
 
 generateMemeBtn.addEventListener("click", generateMeme);
