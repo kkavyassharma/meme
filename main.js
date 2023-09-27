5c3474a37a302c0333927f56b382a55500d3500b
kavya <kkavyassharma@gmail.com>
2023-09-27 09:04:08 +0530

    errors handled

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ const updateDetails = (url, title, author) => {
     memeTitle.innerHTML = title;
     memeAuthor.innerHTML = author;
 };
+try{
+     const generateMeme = () => {
+        fetch("https://meme-api.com/gimme/wholesomememes").then((response) => response.json()).then((data) => {
+            updateDetails(data.url, data.title, data.author)
+        });
+    };
+}
+catch(err){
+console.log(err);
+}
 
 
-const generateMeme = () => {
-    fetch("https://meme-api.com/gimme/wholesomememes").then((response) => response.json()).then((data) => {
-        updateDetails(data.url, data.title, data.author)
-    });
-};
 generateMemeBtn.addEventListener("click", generateMeme);
 generateMeme();
